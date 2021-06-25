ec9f90326b1550b9a61c49682c8fd23de23764a0
john lundgren <john.lundgren.lexicon@gmail.com>
2021-06-25 10:04:18 +0200

    Styling changes.

--- src/components/GroceryDetails.js
+++ src/components/GroceryDetails.js
@@ -8,9 +8,7 @@ const GroceryDetails = ({ grocery }) => {
       <div className="w-100 row">
         <div
           className={
-            grocery.ready
-              ? 'col-10 grocery-title marked'
-              : 'col-10 grocery-title'
+            grocery.ready ? 'col-6 grocery-title marked' : 'col-6 grocery-title'
           }
           onClick={() => {
             dispatch({ type: 'TOGGLE_GROCERY', payload: { id: grocery.id } });
@@ -18,15 +16,67 @@ const GroceryDetails = ({ grocery }) => {
         >
           {grocery.title}
         </div>
-        <div className="col-2">
+        <div className="col-6">
+          <button
+            onClick={() => {}}
+            className="btn btn-success me-3"
+            data-bs-toggle="modal"
+            data-bs-target="#editGroceryModal"
+          >
+            Edit
+          </button>
           <button
             onClick={() =>
               dispatch({ type: 'REMOVE_GROCERY', payload: { id: grocery.id } })
             }
-            className="btn btn-sm btn-danger"
+            className="btn btn-danger me-3"
           >
             X
           </button>
+
+          <div
+            className="modal fade"
+            id="editGroceryModal"
+            tabIndex="-1"
+            aria-labelledby="exampleModalLabel"
+            aria-hidden="true"
+          >
+            <div className="modal-dialog">
+              <div className="modal-content bg-success text-light">
+                <div className="modal-header">
+                  <h5 className="modal-title" id="exampleModalLabel">
+                    Edit Grocery
+                  </h5>
+                  <button
+                    type="button"
+                    className="btn-close"
+                    data-bs-dismiss="modal"
+                    aria-label="Close"
+                  ></button>
+                </div>
+                <div className="modal-body">
+                  <input
+                    className="form-control"
+                    type="text"
+                    name="groceryTitle"
+                    id="groceryTitle"
+                  />
+                </div>
+                <div className="modal-footer">
+                  <button
+                    type="button"
+                    className="btn btn-secondary"
+                    data-bs-dismiss="modal"
+                  >
+                    Close
+                  </button>
+                  <button type="button" className="btn btn-secondary">
+                    Save changes
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </li>
