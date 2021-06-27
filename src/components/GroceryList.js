04f1a561b82496734eb1e580f613899ef2e59722
John Lundgren <john.lundgren.lexicon@gmail.com>
2021-06-27 09:03:09 +0200

    Sort by completion functionality.

--- src/components/GroceryList.js
+++ src/components/GroceryList.js
@@ -1,14 +1,23 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { GroceryContext } from '../contexts/GroceryContext';
 import GroceryDetails from './GroceryDetails';
 
 const GroceryList = () => {
   const { state } = useContext(GroceryContext);
+  const [sortedByCompletion, setSortedByCompletion] = useState(null);
+  useEffect(() => {
+    if(!state.groceries)
+      return;
+    const sortedGroceries = state.groceries.sort((a, b) => {
+      return a.ready === b.ready ? 0 : a.ready ? 1 : -1;
+    });
+    setSortedByCompletion(sortedGroceries);
+  }, [state.groceries])
   return (
     <div className="my-4">
-      {state.groceries && (
+      {sortedByCompletion && (
         <ul className="grocery-list list-group list-group-flush">
-          {state.groceries.map((g) => (
+          {sortedByCompletion.map((g) => (
             <GroceryDetails key={g.id} grocery={g}></GroceryDetails>
           ))}
         </ul>
